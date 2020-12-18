3720bd34fc0ac14da4382fa7d74c24aaf83484f8
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-12-18 21:24:54 +0600

    /

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -11,4 +11,37 @@ class SignUpController extends Controller
   {
     return view('SignUp.index');
   }
+
+  public function create(Request $request)
+    {
+        $validate = Validator:: make($request->all(),[
+            'username' => 'required',
+            'password' =>'required|min:4',
+            'confirmpassword' =>'required|same:password',
+            'fullname' =>'required',
+            'design' =>'required',
+            'email' =>'required',
+            'age' =>'required|min:1',
+        ]);
+        if($validate->fails())
+        {
+            return back()->with('errors',$validate->errors())->withInput();
+        }
+        else
+        {
+            $LID = $request->username;
+            $pass= $request->confirmpassword;
+            $SID = '0';
+
+            $login_table = DB::table('log_in')->insert(['LID'=>$LID,'SID'=>$SID,'PASS'=>$pass]);
+
+            if($login_table== TRUE)
+            {
+                DB::table('customer')->insert(['cusid'=>$LID,'name'=>$request->fullname, 'design'=>$request->design,'email'=>$request->email,'mobile'=>$request->mobile,'status'=>'2']);
+                DB::table('profile_image')->insert([['UID' => $LID, 'IMAGE' => 'BT_Default_avatar_011211.png']]);
+
+                return redirect()->route('login.index')->with('success','Account Created wait for Admin Verify');
+            }
+        }
+    }
 }
---
+++ resources/views/SignUp/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="{{ URL::to('css/signup.css') }}">
+	<script src="assets/js/validSignup.js"></script>
+</head>
+
+<body>
+	<div class="wrap">
+		<h2>Sign Up</h2>
+		<form method="post" name="myForm">
+			@csrf
+			<input type="text" name="username" placeholder="Username" value="">
+			@foreach($errors->get('username') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+       		@endforeach
+			<input type="Password" name="password" placeholder="Password"><br>
+			@foreach($errors->get('password') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+       		@endforeach
+			<input type="Password" name="confirmpassword" placeholder="Confirm Password"><br>
+			@foreach($errors->get('confirmpassword') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+        	@endforeach
+			<input type="text" name="fullname" placeholder="Full Name" value="">
+			@foreach($errors->get('fullname') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+       		@endforeach
+      <input type="number" name="age" placeholder="Age" value="">
+			@foreach($errors->get('age') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+       		@endforeach
+			<input type="text" name="design" placeholder="Occupation" value="">
+			@foreach($errors->get('design') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+        	@endforeach
+			<input type="email" name="email" placeholder="Email" value="">
+			@foreach($errors->get('email') as $err)
+        	<span style="color: red">{{$err}}</span> <br>
+        	@endforeach
+			<input type="submit" name="REGISTER" value="REGISTER"><br><br>
+
+
+
+			<p><b>Or Log In <a href="{{route('signin')}}">here</a></b></p>
+		</form>
+	</div>
+</body>
+
+</html>
